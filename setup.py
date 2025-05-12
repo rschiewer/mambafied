993b555a281224c83ac9b45e2a108396bbef4d5c
Robin Schiewer <robin.schiewer@ini.rub.de>
2025-05-12 17:42:22 +0100

    update project data with forked repo

--- setup.py
+++ setup.py
@@ -2,17 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='mambafied',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(),
     install_requires=[
         'torch',
     ],
     author='Alexandre TL',
     author_email='alexandretl3434@gmail.com',
+    maintainer='Robin Schiewer',
+    maintainer_email='r.schiewer@gmx.de',
     description='A simple and efficient Mamba implementation in pure PyTorch.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/alxndrTL/mamba.py',
+    url='https://github.com/rschiewer/mambafied',
+    project_urls={
+        'Upstream': 'https://github.com/alxndrTL/mamba.py',
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
